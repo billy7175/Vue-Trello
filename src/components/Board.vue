476914c3d01613d605f0297210e333c9aa862b80
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-10-17 03:08:05 +0900

    router nested loading and fetchData

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,24 +1,36 @@
 <template>
-  <div>Board
-    <div>{{bid}}</div>
-    <router-link :to="`/board/${bid}/card/1`">Card1</router-link>
-    <router-link :to="`/board/${bid}/card/2`">Card2</router-link>
-    <hr>
+  <div>
+    <div>Board : board-id = {{ bid }}</div>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <router-link :to="`/board/${bid}/card/1`">Card1</router-link>
+      <router-link :to="`/board/${bid}/card/2`">Card2</router-link>
+    </div>
+    <hr />
     <router-view></router-view>
   </div>
 </template>
 <script>
 export default {
-  name:'Board',
-  data(){
+  name: "Board",
+  data() {
     return {
-      bid: 0
-    }
+      loading: false,
+      bid: 0,
+    };
   },
-  created(){
-    console.log(this.$route)
-    console.log(this.$route.params.bid)
-    this.bid = this.$route.params.bid
-  }
-}
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.bid = this.$route.params.bid;
+        this.loading = false
+      }, 1000);
+      
+    },
+  },
+};
 </script>
