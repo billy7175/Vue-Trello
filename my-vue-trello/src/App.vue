296628825534e455bbff9f2e421720b541c3dfe9
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-10-16 21:17:58 +0900

    vue init webpack

---
+++ my-vue-trello/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    {{  message }}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      message: 'Hello ?'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
