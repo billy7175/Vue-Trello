3633fba8bbbf8dd0450eac0daa056614057178fe
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-10-17 15:10:30 +0900

    custom Axios Request

---
+++ src/api/index.js
@@ -0,0 +1,30 @@
+
+import axios from 'axios'
+import router from '../router'
+const DOMAIN = 'http://localhost:3000'
+const UNAUTORIZED = 401
+const onUnautorized  = () => {
+  router.push('/login')
+}
+
+
+const request = (method, url, data) => {
+  return axios({
+    method,
+    url:DOMAIN + url,
+    data
+  })
+  .then(result => result.data)
+  .catch(result => {
+    const {status} = result.response
+    console.log('status ', result)
+    if(status === UNAUTORIZED ) return onUnautorized()
+    throw Error(result)
+  })
+}
+
+export const board = {
+  fetch(){
+    return request('get', '/boards')
+  }
+}
