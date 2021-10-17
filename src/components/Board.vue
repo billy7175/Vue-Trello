9858abecff11d5273e9b725e36a64d1c44f1d05c
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-10-17 14:01:48 +0900

    XMLHttpRequest

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,9 +3,13 @@
     <div>Board : board-id = {{ bid }}</div>
     <div v-if="loading">Loading...</div>
     <div v-else>
-      <router-link :to="`/board/${bid}/card/1`">Card1</router-link>
-      <router-link :to="`/board/${bid}/card/2`">Card2</router-link>
+      API result: {{apiRes}}
     </div>
+
+    <ul>
+      <li><router-link :to="`/board/${bid}/card/1`">Card1</router-link></li>
+      <li><router-link :to="`/board/${bid}/card/2`">Card2</router-link></li>
+    </ul>
     <hr />
     <router-view></router-view>
   </div>
@@ -17,7 +21,8 @@ export default {
     return {
       loading: false,
       bid: 0,
-    };
+      apiRes: ''
+    }
   },
   created() {
     this.fetchData();
@@ -25,6 +30,17 @@ export default {
   methods: {
     fetchData() {
       this.loading = true;
+      const req = new XMLHttpRequest()
+      req.open('GET','http://localhost:3000/health');
+      req.send()
+      req.addEventListener('load', () => {
+        this.loading = false
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response : JSON.parse(req.response)
+        }
+      })
       setTimeout(() => {
         this.bid = this.$route.params.bid;
         this.loading = false
