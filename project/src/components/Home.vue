b0dd1585785d17e35b190cf21a9740c07c8d12f2
billy7175 <whgus439@gmail.com>
2022-05-30 15:28:53 +0900

    board list styling

--- project/src/components/Home.vue
+++ project/src/components/Home.vue
@@ -1,49 +1,103 @@
 <template>
   <div>
-    Board List
-    <div v-if="loading">loading...</div>
-    <div v-else>
-      <pre>{{ boards }}</pre>
+    <div class="home-title">Personal Boards</div>
+    <div class="board-list" ref="boardList">
+      <div class="board-item" v-for="b in boards" :key="b.id" 
+        :data-bgcolor="b.bgColor" ref="boardItem">
+        <router-link :to="`/b/${b.id}`">
+          <div class="board-item-title">{{b.title}}</div>
+        </router-link>
+      </div>
+      <div class="board-item board-item-new">
+        <a class="new-board-btn" href="" @click.prevent="addBoard">
+          Create new board...
+        </a>
+      </div>
     </div>
-    <div v-if="error">
-      <pre>{{ error }}</pre>
-    </div>
-    <ul>
-      <router-link to="/b/1">b1</router-link>
-      <router-link to="/b/2">b2</router-link>
-      <router-link to="/b/3">b3</router-link>
-    </ul>
   </div>
 </template>
 
 <script>
-import { board } from "@/api";
+import {board} from '../api'
+
 export default {
-  created() {
-    this.fetchData();
-  },
-  methods: {
-    fetchData() {
-      board
-        .fetch()
-        .then((res) => {
-          console.log('#boards', res)
-          this.boards = res.list;
-        })
-        .finally(() => {
-          this.loading = false;
-        });
-    },
-  },
   data() {
     return {
       loading: false,
       boards: [],
-      error: "",
-    };
+      error: ''
+    }
   },
-};
+  created() {
+    this.fetchData()
+  },
+  updated() {
+    this.$refs.boardItem.forEach(el => {
+      el.style.backgroundColor = el.dataset.bgcolor
+    })
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      board.fetch()
+        .then(data => {
+          this.boards = data.list
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+    addBoard() {
+      console.log('addBoard()')
+    }
+  }
+}
 </script>
 
 <style>
+.home-title {
+  padding: 10px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.board-list {
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.board-item {
+  width: 23%;
+  height: 100px;
+  margin: 0 2% 20px 0;
+  border-radius: 3px;
+}
+.board-item-new {
+  background-color: #ddd;
+}
+.board-item a {
+  text-decoration: none;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.board-item a:hover,
+.board-item a:focus {
+  background-color: rgba(0,0,0, .1);
+  color: #666;
+}
+.board-item-title {
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  padding: 10px;
+}
+.board-item a.new-board-btn {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+  height: 100px;
+  width: inherit;
+  color: #888;
+  font-weight: 700;
+}
 </style>
