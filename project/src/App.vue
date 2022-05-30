3b01a6d2307a390dfc6549445aa912fca0d6d2d3
billy7175 <whgus439@gmail.com>
2022-05-30 12:15:53 +0900

    vue router-link

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
+    <NavBar></NavBar>
     <router-view></router-view>
     <!-- <router-view></router-view> -->
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/Navbar'
 export default {
   name: "App",
+  components : {
+    NavBar
+  }
 };
 </script>
 
--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from '../App.vue'
+// import App from '../App.vue'
+import Home from '@/components/Home'
 import Login from '@/components/Login'
 import NotFound from '@/components/NotFound'
 
@@ -12,7 +13,7 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: App },
+    { path: '/', component: Home },
     { path: '/login', component: Login },
     { path: '*', component: NotFound }
   ]
