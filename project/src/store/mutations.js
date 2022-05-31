12bce1d65040708f3d08382ae0dd6d21a2e13dea
조현 <hcho@growthsoft.co.kr>
2022-05-31 14:35:47 +0900

    vuex refactoring

---
+++ project/src/store/mutations.js
@@ -0,0 +1,21 @@
+const mutations = {
+    LOGIN(state, token){
+        if(!token) return 
+        state.token = token
+        localStorage.setItem('token', token)
+        api.setAuthInHeader(token)
+    },
+    LOGOUT(state){
+        state.token = null
+        delete localStorage.token
+        api.setAuthInHeader(null)
+    },
+    SET_IS_ADD_BOARD(state, toggle){
+        state.isAddBoard = toggle
+    },
+    SET_BOARDS(state, boards){
+        state.boards = boards
+    }
+}
+
+export default mutations
