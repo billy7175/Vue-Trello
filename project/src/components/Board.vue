943d4806b559bf24f6dbc572a722ff3bd704fc25
조현 <hcho@growthsoft.co.kr>
2022-05-31 15:28:32 +0900

    vuex FETCH_BOARD

--- project/src/components/Board.vue
+++ project/src/components/Board.vue
@@ -3,6 +3,9 @@
     Board
     <div v-if="loading">Board is loading....</div>
     <div v-else>
+      <pre>
+        {{ board }}
+      </pre>
       <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
       <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
     </div>
@@ -12,7 +15,14 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 export default {
+  computed: {
+    ...mapState([
+      'board'
+    ])
+  },
   data() {
     return {
       loading: false,
@@ -26,12 +36,16 @@ export default {
     },
   },
   methods: {
+    ...mapActions([
+      'FETCH_BOARD'
+    ]),
     fetchData() {
       this.loading = true;
-      setTimeout(() => {
-        this.bid = this.$route.params.bid;
-        this.loading = false;
-      }, 1000);
+      this.FETCH_BOARD({ id: this.$route.params.bid }).then(() => this.loading = false)
+      // setTimeout(() => {
+      //   this.bid = this.$route.params.bid;
+      //   this.loading = false;
+      // }, 1000);
     },
   },
 };
